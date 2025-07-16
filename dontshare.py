6b76efc1a43a87e5ee2fe4bd22671bfba674ba8d
Anatolii Poliarush <tanakahiroshi11417@gmail.com>
2025-07-17 00:22:12 +0800

    Update dontshare.py

--- dontshare.py
+++ dontshare.py
@@ -1,9 +1,9 @@
 key = ''  # Solana test wallet private key
-HELIUS_API_KEY = "8013b84f-3db2-4e68-b05e-9f6a98159afb"
+HELIUS_API_KEY = "Your Helius api key"
 WALLET_ADDRESS = "" #Your wallet address
-chat_id='7602918075'
-sol=0.01
+chat_id='telegram chat id'
+sol="Sol amount"
 
 
-BOT_TOKEN="7650230323:AAG0sNdKr0vXvQ4bbSSnixagSG8PFuNP6qg"
-birdeye='efbd0c63e8df4dcc91e8002fbcb8f241'
+BOT_TOKEN="telegram bot ID"
+birdeye='Birdeye api key'
